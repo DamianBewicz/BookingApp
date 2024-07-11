a86f9708a181e9e05a34ddb9f78b8b9c84a7e8af
Damian <bewicz.damian@gmail.com>
2024-07-11 20:45:05 +0200

    Created application db context with users db set

---
+++ BookingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BookingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
