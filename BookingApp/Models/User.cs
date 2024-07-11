0fe4ed9e097cbc6bfafcf89b268f842b979c2278
Damian <bewicz.damian@gmail.com>
2024-07-11 20:44:09 +0200

    Add starting user model which later is going to be replaced with identity user

---
+++ BookingApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApp.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255, ErrorMessage = "Must be between 5 and 255 characters", MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
