355b48bb17fbac1769aa5090ba72de34e568c2a4
Damian <bewicz.damian@gmail.com>
2024-08-25 19:54:40 +0200

    Add change password cshtml page, change logic for change password controller and add required messages for controller model

--- BookingApp/Controllers/UserController.cs
+++ BookingApp/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using BookingApp.Models;
 using BookingApp.ViewModels.User;
 using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace BookingApp.Controllers
 {
@@ -97,5 +98,50 @@ namespace BookingApp.Controllers
 
 			return View(registerViewModel);
 		}
-	}
+
+		public IActionResult ChangePassword()
+		{
+            return View();
+		}
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            ClaimsPrincipal currentUser = this.User;
+            string? id = _userManager.GetUserId(User);
+            User? user = await _userManager.FindByIdAsync(id ?? "");
+
+            if (user != null)
+            {
+                bool isSameAsOldPassword = await _userManager.CheckPasswordAsync(user, model.NewPassword);
+                if (isSameAsOldPassword)
+                {
+                    ModelState.AddModelError("NewPassword", "New password can't be the same as the old password");
+                    return View(model);
+                }
+
+                IdentityResult result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                if (result.Succeeded)
+                {
+                    await _signInManager.RefreshSignInAsync(user);
+                    TempData["SuccessMessage"] = "Your password has been succesfully changed";
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            else
+            {
+                return NotFound("Nie znaleziono użytkownika.");
+            }
+            return View();
+        }
+    }
 }
---
+++ BookingApp/ViewModels/User/ChangePasswordViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookingApp.ViewModels.User
+{
+	public class ChangePasswordViewModel
+	{
+		[Required(ErrorMessage = "Old password is required")]
+		[DataType(DataType.Password)]
+		public string? OldPassword { get; set; }
+		[Required(ErrorMessage = "New password is required")]
+		[DataType(DataType.Password)]
+		public string? NewPassword { get; set; }
+		[Required(ErrorMessage = "Confirm Password is required")]
+		[DataType(DataType.Password)]
+		[Compare("NewPassword", ErrorMessage = "Passwords do not match")]
+		public string? ConfirmPassword { get; set; }
+
+	}
+}
